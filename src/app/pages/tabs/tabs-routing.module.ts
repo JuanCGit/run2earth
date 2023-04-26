d40916fe8fa25dbe4fbd4780c4bb4e9a172fdc1d
JuanCGit <juancasanovagago@gmail.com>
2023-04-26 17:18:44 +0200

    feat(global): Refactored project composition

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -8,27 +8,27 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'page1',
+        loadChildren: () => import('../page1/page1.module').then(m => m.Page1PageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'page2',
+        loadChildren: () => import('../page2/page2.module').then(m => m.Page2PageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'page3',
+        loadChildren: () => import('../page3/page3.module').then(m => m.Page3PageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/page1',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/page1',
     pathMatch: 'full'
   }
 ];
